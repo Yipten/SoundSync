96ea46a4946298a175f48f4880abd7e900d4a7f1
Andrew S <yipten@outlook.com>
2019-07-03 20:57:32 -0400

    -more user-friendly -add code comments -now it actually works! (although not fully tested)

--- SoundSync/MainWindow.xaml.cs
+++ SoundSync/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System;
 using System.IO;
 using System.Net;
-using System.Net.Cache;
 using System.Text.RegularExpressions;
 using System.Timers;
 using System.Windows;
@@ -14,6 +13,8 @@ namespace SoundSync {
 		private DateTime start;
 		private Timer timer;
 
+		private string fileName;
+
 		public MainWindow() {
 			InitializeComponent();
 
@@ -22,7 +23,7 @@ namespace SoundSync {
 			music = new MediaPlayer();
 			music.MediaEnded += Music_MediaEnded;
 			timer = new Timer();
-			timer.AutoReset = true;
+			timer.AutoReset = false;
 			timer.Elapsed += Timer_Elapsed;
 			timer.Stop();
 
@@ -47,56 +48,47 @@ namespace SoundSync {
 			UpdateStatus("idle");
 		}
 
-		private void Timer_Elapsed(object sender, ElapsedEventArgs e) {     //TODO: why is this not working? maybe because of threads or something?
-			DateTime now = GetDateTime();
-			int seconds = now.Second;
-			Output.Text = seconds.ToString();
-			if (seconds == 0)
-				// start the music!
-				music.Play();
-			else if (seconds >= 59)
-				timer.Interval = 1;
-			else if (seconds >= 55)
-				timer.Interval = 100;
-			else if (seconds >= 45)
-				timer.Interval = 1000;
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e) {
+			//stop the timer
 			timer.Stop();
-			if (seconds != 0)
-				timer.Start();
+			//start the music!
+			Dispatcher.Invoke(() => {
+				music.Play();
+				UpdateStatus("playing \"" + fileName + "\"");
+			});
 		}
 
 		private void Music_MediaEnded(object sender, EventArgs e) {
+			//reset MediaPlayer and buttons after music ends
+			music.Stop();
 			LoadButton.IsEnabled = true;
 			StopButton.IsEnabled = false;
 			WaitButton.IsEnabled = true;
-			UpdateStatus("idle");
+			Date.IsEnabled = true;
+			Hour.IsEnabled = true;
+			Minute.IsEnabled = true;
+			UpdateStatus("\"" + fileName + "\" ended");
 		}
 
 		private DateTime GetDateTime() {
-			//get time and date from the Internet
-			DateTime dateTime = DateTime.MinValue;
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://nist.time.gov/actualtime.cgi?lzbc=siqm9b");
-			request.Method = "GET";
-			request.Accept = "text/html, application/xhtml+xml, */*";
-			request.UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
-			request.ContentType = "application/x-www-form-urlencoded";
-			request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore); //No caching
-			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-			if (response.StatusCode == HttpStatusCode.OK) {
-				StreamReader stream = new StreamReader(response.GetResponseStream());
+			//get time and date from the Internet, which is the same place I found this code :)
+			try {
+				StreamReader stream = new StreamReader(WebRequest.Create("https://nist.time.gov/actualtime.cgi?lzbc=siqm9b").GetResponse().GetResponseStream());
 				string html = stream.ReadToEnd();
 				string time = Regex.Match(html, @"(?<=\btime="")[^""]*").Value;
 				double milliseconds = Convert.ToInt64(time) / 1000.0;
-				dateTime = new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+				return new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+			} catch {
+				MessageBox.Show("No Internet connection. Local time will be used. ", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				return DateTime.Now;
 			}
-			return dateTime;
 
-			//this one kind of works but not always
+			//this one works but sometimes doesn't
 			//return DateTime.ParseExact(WebRequest.Create("https://www.duckduckgo.com").GetResponse().Headers["date"], "ddd, dd MMM yyyy HH:mm:ss 'GMT'", CultureInfo.InvariantCulture.DateTimeFormat, DateTimeStyles.AssumeUniversal);
-
 		}
 
 		private void UpdateStatus(string text) {
+			//change text displayed in status text box for diagnostics and to inform the user
 			Status.Text = "Status: " + text;
 		}
 
@@ -105,11 +97,14 @@ namespace SoundSync {
 			OpenFileDialog openFileDialog = new OpenFileDialog {
 				Filter = "Sound Files|*.wav; *.mp3|All Files|*.*"
 			};
+			//prompt user to open a sound file from their computer
 			bool? hasFile = openFileDialog.ShowDialog();
+			//if a file has been selected, load it into the MediaPlayer
 			if (hasFile == true) {
 				music.Open(new Uri(openFileDialog.FileName, UriKind.Absolute));
-				UpdateStatus("loaded \"" + openFileDialog.SafeFileName + "\"");
 				WaitButton.IsEnabled = true;
+				fileName = openFileDialog.SafeFileName;
+				UpdateStatus("loaded \"" + fileName + "\"");
 			} else
 				UpdateStatus("idle");
 		}
@@ -118,19 +113,29 @@ namespace SoundSync {
 			LoadButton.IsEnabled = false;
 			StopButton.IsEnabled = true;
 			WaitButton.IsEnabled = false;
+			Date.IsEnabled = false;
+			Hour.IsEnabled = false;
+			Minute.IsEnabled = false;
+			//get date and time from selected values
 			DateTime selectedDate = Date.SelectedDate.Value;
 			start = new DateTime(selectedDate.Year, selectedDate.Month, selectedDate.Day, (int)Hour.SelectedItem + (AM.IsChecked == true != ((int)Hour.SelectedItem == 12) ? 0 : 12), (int)Minute.SelectedItem, 0);
-			timer.Interval = start.Subtract(GetDateTime()).TotalMilliseconds - 10000;
+			//set timer interval as time until start time occurs
+			timer.Interval = start.Subtract(GetDateTime()).TotalMilliseconds;
 			timer.Start();
+			UpdateStatus("starting at " + start.ToShortTimeString() + " on " + start.ToShortDateString());
 		}
 
 		private void StopButton_Click(object sender, RoutedEventArgs e) {
+			//stop music and timer
 			music.Stop();
 			timer.Stop();
 			LoadButton.IsEnabled = true;
 			StopButton.IsEnabled = false;
 			WaitButton.IsEnabled = true;
-			UpdateStatus("idle");
+			Date.IsEnabled = true;
+			Hour.IsEnabled = true;
+			Minute.IsEnabled = true;
+			UpdateStatus("loaded \"" + fileName + "\"");
 		}
 	}
 }
